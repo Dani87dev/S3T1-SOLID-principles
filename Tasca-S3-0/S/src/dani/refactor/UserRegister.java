d9d2be242767005a14ae5b8c9bbf2f8f8fdce397
Daniel Vilà <danivila.dev@gmail.com>
2026-02-19 13:08:10 +0100

    implement basic classes to refactorize task S level 1

--- Tasca-S3-0/S/src/dani/refactor/UserRegister.java
+++ Tasca-S3-0/S/src/dani/refactor/UserRegister.java
@@ -1,30 +1,27 @@
 package dani.refactor;
 
-public class UserRegister implements Registable{
+public class UserRegister {
 
     private User user;
 
-    public UserRegister(User user){
+
+    public UserRegister(User user) {
         this.user = user;
     }
 
-    public void registrar(User user){
-
-        if (user.getEmail() == null || !user.getEmail().contains("@") || !user.getEmail().contains(".")) {
-            throw new IllegalArgumentException("Invalid email address.");
-        }
-
-        if (user.getPassword() == null || user.getPassword().length() < 8 || !user.getPassword().matches(".*[A-Z].*")) {
-            throw new IllegalArgumentException("Password must be at least 8 characters long and contain an uppercase letter.");
-        }
-
+    /*
         System.out.println("📧 Sending confirmation email to: " + user.getEmail());
 
-        boolean userConfirmed = true;
-        if (!userConfirmed) {
-            System.out.println("⚠️ User did not confirm registration.");
-            return;
+
         }
+        */
+    //Registro
+    public void register() {
+
+        UserValidator validator1 = new UserValidator();
+        validator1.validate(user);
+
 
     }
+}
 }
