27179cac39a275e6e8b49915b8d3208af12ef9b9
Daniel Vilà <danivila.dev@gmail.com>
2026-03-05 18:00:32 +0100

    feat: upload tests fro D and S tasks from SOLID project

--- Tasca-S3-0/src/D/test/mySolution/MySqlTest.java
+++ Tasca-S3-0/src/D/test/mySolution/MySqlTest.java
@@ -1,6 +1,20 @@
 package mySolution;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class MySqlTest {
 
-    //Test d'Integració amb MySQL:
+    @Test
+    void checkMySQLClassisWorking(){
+
+        Saveable mySqlDdbb = new MySQL();
+
+        ServicePerson testService = new ServicePerson(mySqlDdbb);
+        Person testPerson = new Person("MySQL_test_user");
+
+        assertDoesNotThrow(()->testService.SaveData(testPerson));
+
+    }
 }
--- Tasca-S3-0/src/D/test/mySolution/ServicePersonTest.java
+++ Tasca-S3-0/src/D/test/mySolution/ServicePersonTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ServicePersonTest {
-    
+
     @Test
     void checkWorksWithOtherDdbbSystems() {
 
